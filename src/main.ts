04bbcb95056d0f381a8ce59f02ed572bf8d7a57a
S.M. Mohibullah Bukhari <mohibullah.bukhari@dotfoods.com>
2024-11-02 10:13:00 +0500

    Added Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { logger: ['error', 'warn', 'fatal', 'verbose'] },
+  );
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`\n\n\nApplication is running on: ${await app.getUrl()}\n\n\n`);
 }
 bootstrap();
